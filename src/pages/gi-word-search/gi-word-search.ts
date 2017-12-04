2f237b692002b83044183d7248fe9e84b73b9534
moakprojects <molak8@gmail.com>
2017-12-04 23:42:25 +0100

    first game

--- src/pages/gi-word-search/gi-word-search.ts
+++ src/pages/gi-word-search/gi-word-search.ts
@@ -13,14 +13,54 @@ import { NavController, NavParams } from 'ionic-angular';
   templateUrl: 'gi-word-search.html',
 })
 export class GiWordSearchPage {
-
+  typedWord : String;
+  answer : String = 'MJOLNIR';
+  selectedLetters : String[] = [];
+  answered : Boolean = false;
   constructor(public navCtrl: NavController, public navParams: NavParams) {
   }
 
-  tapped(e) {
-    console.log(e);
-    document.getElementById(e).style.backgroundColor = "#ff993d";
-    
+  tapDown(e) {
+    if(!this.answered) {
+      document.getElementById(e).style.backgroundColor = "#ff993d";
+      document.getElementById(e).getElementsByTagName("p")[0].style.color = "#2A2F39";
+    }
+  }
+
+  tapUp(e, value) {
+    if(!this.answered) {
+      if(this.typedWord) {
+        if(this.selectedLetters.indexOf(e) === -1) {
+          this.selectedLetters.push(e);
+          this.typedWord += value;
+          if(value === this.answer[this.selectedLetters.length - 1]) {
+            if(this.typedWord === this.answer) {
+              console.log(this.typedWord);
+              console.log(this.answer);
+              this.answered = true;
+              console.log(this.answered);
+            }
+          } else {
+            for(let i =0; i < this.selectedLetters.length; i++) {
+              document.getElementById(this.selectedLetters[i].toString()).style.backgroundColor = "#2A2F39";
+              document.getElementById(this.selectedLetters[i].toString()).getElementsByTagName('p')[0].style.color = "#ff993d";
+            }
+            this.selectedLetters = [];
+            this.typedWord = undefined;
+          }
+        }
+      } else {
+        if(value === this.answer[0]) {
+          this.typedWord = value;
+          this.selectedLetters.push(e);
+        } else {
+          document.getElementById(e).style.backgroundColor = "#2A2F39";
+          document.getElementById(e).getElementsByTagName("p")[0].style.color = "#ff993d";
+        }
+      }
+  
+      console.log("type: " + this.typedWord);
+    }
   }
 
 }
